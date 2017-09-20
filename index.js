5b67887875e56593f419e12d9dc3ad6551a1dde0
Unknown <ro0504008@otc.edu>
2017-09-20 13:23:15 -0500

    close

--- index.js
+++ index.js
@@ -2,32 +2,52 @@ const inquirer = require('inquirer')
 const chalk = require('chalk')
 
 
+console.log('Welcome to Night Blades Pizzaria')
 
-var question = [{
-  name: 'tacoIngredient',
-  message: 'Whats another tasty thing to add to your taco?',
-  default : ''
-}]
+var questions = [{
+   message: "Have you ordered through us before?",
+   name:  'member',
+   type: "list",
+   choices:['yes','no']
+  },{
+    message: "Address for the order??",
+   name:  'address',
+      type: "input",
 
-var tacoIngredients = [];
+  },{
+    message: "How many?",
+   name:  'number',
+      type: "list",
+   choices:['1','2','3','4','5','6',]
+  },{
+    message: "Size?",
+   name:  'size',
+      type: "list",
+   choices:['Small','Medium','Large', 'Extra large', 'Various']
+  },{
+    message: "How do you want it cut?",
+   name:  'cut',
+      type: "list",
+   choices:['yes','no']
+  },{
+    message: "Addons?",
+   name:  'extras',
+      type: "list",
+   choices:['yes','no']
+ 
+  }]
 
-function finalAction(){
+inquirer.prompt(questions).then(function({member,address,number,size,cut,extras}){
 
-  console.log('Make a taco with: =>' + tacoIngredients)
+if (member = yes){
+  var questions = [{
+   message: "Enter member number",
+   name:  'mnumb',
+   type: "input",
+   
+  }]
+
 }
-
-function askOrPerformFinalAction(answer) {
-
-  ingredientsList.push(answers.tacoIngredient);
-
-  if (!answers.tacoIngredient) {
-    finalAction(tacoIngredients);
-    return;
-  }
-
-  return inquirer.prompt(question, askOrPerformFinalAction);
-}
-
-inquirer.prompt(question, askOrPerformFinalAction);
-
-console.log('tacoIngredient')
+inquirer.prompt(questions).then(function({member,mnumb,address,number,size,cut,extras}){
+console.log(member,mnumb,address,number,size,cut,extras)
+})})
